058a3f90d8a5438ba85c1584d1bc19fee5b5cf1c
chriz001 <designategroup37@192-168-1-2.tpgi.com.au>
2015-11-30 15:35:00 +1100

    gh-pages demo

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,14 +2,12 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'source-map',
   entry: [
     './src/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
